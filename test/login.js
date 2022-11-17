b66d042dd984630c75ae7dd1a8d42c29e345eaff
Matthew Brunson <matt0808@byui.edu>
2022-11-17 11:17:51 -0700

    changed login.js test

--- test/login.js
+++ test/login.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 
 const login = async ()=> {
-  const tokenResponse = await fetch("https://dev.stedi.me/login",{
+  const tokenResponse = await fetch("https://matthewbrunson.cit262.com/login",{
     method: "POST",
     body:JSON.stringify({
       userName: "bru17005@byui.edu",
